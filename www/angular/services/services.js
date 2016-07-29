52084d456f110c23d0eeef891bfd2db418db738e
S Vinesh <svinesh3691@gmail.com>
2016-07-29 08:38:55 +0530

    Success offlinr

--- www/angular/services/services.js
+++ www/angular/services/services.js
@@ -22,11 +22,11 @@ app.service("fns", ['$http','C', function( $http , C ) {
 
     // Creating the table
 	this.createTables   = function() {
-			this.db.transaction(function(tx){
-				   			tx.executeSql( 'DROP TABLE sales');
-				   			// tx.executeSql( 'DROP TABLE Test');
-				   			console.log('Drop');
-			});
+			// this.db.transaction(function(tx){
+			// 	   			tx.executeSql( 'DROP TABLE sales');
+			// 	   			// tx.executeSql( 'DROP TABLE Test');
+			// 	   			console.log('Drop');
+			// });
 			var sales_table_query 	= 'CREATE TABLE IF NOT EXISTS sales (sales_id INTEGER PRIMARY KEY AUTOINCREMENT,id int, sale_date DATE, credit DATE, cash int, card TEXT,company_id int,company_name TEXT,amount_total int,payables int,amount_avg int,close_value int)';
 			this.query(sales_table_query,[],function(res){
 				console.log(res);
